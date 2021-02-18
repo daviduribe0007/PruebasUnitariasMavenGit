dec96f699d5482a47c8e42b171788e167267c6a8
davidUribe <david@gmail.com>
2021-02-18 14:20:24 -0500

    Se prueba con el assertEquals y se agrega el displayname

--- src/test/java/co/com/sofka/app/calculator/BasicCalculatorTest.java
+++ src/test/java/co/com/sofka/app/calculator/BasicCalculatorTest.java
@@ -1,5 +1,6 @@
 package co.com.sofka.app.calculator;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,13 +10,18 @@ public class BasicCalculatorTest {
     private final BasicCalculator basicCalculator = new BasicCalculator();
 
     @Test
+    @DisplayName("Testing sum: 1+1=2")
     public void sum() {
         // Arrange
         Long number1 = 1L;
         Long number2 = 1L;
+        Long expectedValue = 2L;
 
         // Act
-        basicCalculator.sum(number1, number2);
+        Long result = basicCalculator.sum(number1, number2);
+
+        // Assert
+        assertEquals(expectedValue, result);
     }
 
 
