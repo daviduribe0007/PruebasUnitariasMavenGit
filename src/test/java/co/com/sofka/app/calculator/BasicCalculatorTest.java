f5a34451a9c2dee39395289b7d51fe343ad04f2b
davidUribe <david@gmail.com>
2021-02-18 14:14:30 -0500

    Se agrega la clase BasicCalculatorTest

---
+++ src/test/java/co/com/sofka/app/calculator/BasicCalculatorTest.java
@@ -0,0 +1,24 @@
+package co.com.sofka.app.calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BasicCalculatorTest {
+
+    @Test
+    void sum() {
+    }
+
+    @Test
+    void subtraction() {
+    }
+
+    @Test
+    void multiplication() {
+    }
+
+    @Test
+    void division() {
+    }
+}
